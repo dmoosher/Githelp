fadb2024cb26d2e33261050d389f2fa918c34cd5
dm <dpmdpm@msn.com>
2021-11-23 13:33:09 -0500

    Removed Redux

--- app/main.js
+++ app/main.js
@@ -2,9 +2,10 @@
 // (But if you really want to, no one's gonna stop you. ¯\_(ツ)_/¯)
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
+// Don't need redux
+//import { Provider } from 'react-redux';
 
-import store from './store';
+//import store from './store';
 import Routes from './components/Routes';
 
 render(
